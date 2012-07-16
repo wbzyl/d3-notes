7f29057003883d0044b640de4daa0a4006a8d191
Wlodek Bzyl <matwb@univ.gda.pl>
2012-07-16 17:51:30 +0200

    turnstile traffic example

--- getting-started-with-d3/bus_perf.js
+++ getting-started-with-d3/bus_perf.js
@@ -1,7 +1,7 @@
 function draw(data) {
   "use strict";
 
-  var margin = 50,
+  var margin = 60,
     width = 700,
     height = 300;
 
@@ -27,7 +27,7 @@ function draw(data) {
       .attr("cy", function(d) { return y_scale(d.dist_between_fail); })
       .attr("r", 5);
 
-  // Adding axes
+  // adding axes
 
   var x_axis = d3.svg.axis().scale(x_scale);
 
@@ -35,6 +35,26 @@ function draw(data) {
     .append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + (height - margin) + ")")
-      .call(x_axis);
+    .call(x_axis);
+
+  var y_axis = d3.svg.axis().scale(y_scale).orient("left");
+
+  d3.select("svg")
+    .append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + margin + ", 0 )")
+    .call(y_axis);
+
+  // axis titles
+  d3.select(".x.axis")
+    .append("text")
+      .text("collisions with injury (per million miles)")
+      .attr("x", (width / 2) - margin)
+      .attr("y", margin / 1.5);
+
+  d3.select(".y.axis")
+    .append("text")
+      .text("mean distance between failure (miles)")
+      .attr("transform", "rotate (-90, -45, 0) translate(-285)");
 
 }
---
+++ getting-started-with-d3/turnstile_traffic.js
@@ -0,0 +1,95 @@
+function draw(data) {
+  "use strict";
+
+  var margin = 40,
+      width = 700 - margin,
+      height = 300 - margin;
+
+  d3.select("body")
+    .append("svg")
+      .attr("width", width + margin)
+      .attr("height", height + margin)
+    .append("g")
+      .attr("class", "chart");
+
+  d3.select("svg")
+    .selectAll("circle.times_square")
+      .data(data.times_square)
+    .enter().append("circle")
+      .attr("class", "times_square");
+
+  d3.select("svg")
+    .selectAll("circle.grand_central")
+      .data(data.grand_central)
+    .enter().append("circle")
+      .attr("class", "grand_central");
+
+  var times = data.times_square.concat(data.grand_central);
+
+  var count_extent = d3.extent(times, function(d) { return d.count; });
+
+  var count_scale = d3.scale.linear()
+    .domain(count_extent)
+    .range([height, margin]);
+
+  var time_extent = d3.extent(times, function(d) { return d.time; });
+
+  var time_scale = d3.time.scale()
+    .domain(time_extent)
+    .range([margin, width]);
+
+  d3.selectAll("circle")
+      .attr("cx", function(d) { return time_scale(d.time); })
+      .attr("cy", function(d) { return count_scale(d.count); })
+      .attr("r", 3);
+
+  // adding axes
+
+  var time_axis = d3.svg.axis().scale(time_scale);
+
+  d3.select("svg")
+    .append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+    .call(time_axis);
+
+  var count_axis = d3.svg.axis().scale(count_scale).orient("left");
+
+  d3.select("svg")
+    .append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + margin + ",0)")
+    .call(count_axis);
+
+  // paths
+
+  var line = d3.svg.line()
+    .x(function(d) { return time_scale(d.time); })
+    .y(function(d) { return count_scale(d.count); });
+
+  d3.select("svg")
+    .append("path")
+      .attr("d", line(data.times_square))
+      .attr("class", "times_square");
+
+  d3.select("svg")
+    .append("path")
+      .attr("d", line(data.grand_central))
+      .attr("class", "grand_central");
+
+  // axis labels
+
+  d3.select(".y.axis")
+    .append("text")
+      .text("mean number of turnstile revolutions")
+      .attr("transform", "rotate (-90, -32, 0) translate(-275)")
+      .attr("x", 20)
+      .attr("y", 0);
+
+  d3.select(".x.axis")
+    .append("text")
+      .text("time")
+    .attr("x", function() { return (width / 1.6) -  margin; })
+      .attr("y", margin / 1.2);
+
+}
