909ddbfe37ddb69e3344bed1dce0e73628c7faf3
Wlodek Bzyl <matwb@univ.gda.pl>
2012-07-17 20:24:55 +0200

    cont.

--- gist/glow.js
+++ gist/glow.js
@@ -2,31 +2,42 @@ function glow() {
   var width = 64, // default width
       height = 64; // default height
 
-  var stdDev = 2;
+  var stdDev = 2,
+      colorMatrix = "0 0 0 1  0  0 0 0 0  0  0 0 0 0  0  0 0 0 1  0";
 
   function my() {
 
     var defs = this.append("defs");
 
     var filter = defs.append("filter")
-      .attr("id", "glow")
+        .attr("id", "glow")
+      .call(function() {
+        this.append("feColorMatrix")
+            .attr("type", "matrix")
+            .attr("values", colorMatrix);
+        this.append("feGaussianBlur")
+             // .attr("in", "SourceGraphics")
+             .attr("stdDeviation", stdDev)
+            .attr("result", "coloredBlur");
+      });
 
-    filter.append("feGaussianBlur")
-      .attr("in", "SourceAlpha")
-      .attr("stdDeviation", stdDev)
-      .attr("result", "blur");
-    filter.append("feOffset")
-      .attr("in", "blur")
-      .attr("dx", 4)
-      .attr("dy", 4)
-      .attr("result", "offsetBlur");
+    // filter.append("feColorMatrix")
+    //     .attr("type", "matrix")
+    //     .attr("values", colorMatrix);
 
-    var feMerge = filter.append("feMerge");
+    // filter.append("feGaussianBlur")
+    //      // .attr("in", "SourceGraphics")
+    //     .attr("stdDeviation", stdDev)
+    //     .attr("result", "coloredBlur");
 
-    feMerge.append("feMergeNode")
-      .attr("in", "offsetBlur")
-    feMerge.append("feMergeNode")
-      .attr("in", "SourceGraphic");
+    filter.append("feMerge")
+      .call(function() {
+        this.append("feMergeNode")
+            .attr("in", "coloredBlur");
+        this.append("feMergeNode")
+            .attr("in", "SourceGraphic");
+      });
+
   }
 
   my.width = function(value) {
